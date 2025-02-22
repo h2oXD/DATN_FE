beac7db290ea9a16e4c7a4952bfd54f2f47e4345
HuuHao <fixbugandcry@gmail.com>
2025-02-22 10:52:30 +0700

    real time

--- src/App.jsx
+++ src/App.jsx
@@ -37,13 +37,9 @@ import OverView from "./pages/Student/OverView/OverView";
 
 
 import RegisterTeacher from "./components/RegisterTeacher";
-import FormRegisterTeacher from "./pages/Auth/FormRegisterTeacher";
 import Course from "./pages/Student/CoursePage/Course";
 import MyCourse from "./pages/Student/CoursePage/MyCourse";
 
-import PaymentCallback from "./pages/Student/BuyCourse/PaymentCallback";
-import RequireQuizParams from "./components/RequireQuizParams";
-import EditQuiz from "./pages/Lecturer/Quiz/EditQuiz";
 
 
 
@@ -72,10 +68,6 @@ function App() {
           <Route
             path="/student/registerTeacher"
             element={<RegisterTeacher />}
-          />
-          <Route
-            path="/student/payment-callback/:course_id"
-            element={<PaymentCallback />}
           />
         </Route>
 
